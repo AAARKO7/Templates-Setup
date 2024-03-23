3257191dca21045756b5eacee21a524b83efdb8c
AAARKO7 <aadhikaryjs2@gmail.com>
2024-03-24 04:46:33 +0600

    Arko

---
+++ templates/template_cp_arko.cpp
@@ -0,0 +1,32 @@
+/*
+    Solver:
+    Date:
+
+*/
+
+/**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main() 
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+ 
+	/*Test case*/
+	int t=1;
+	cin>>t;
+	for(int i=1;i<=t;i++) 
+    {
+		//cout<<"case #"<<i<<": ";
+		solve();
+	}
+ 
+	return 0;
+}
