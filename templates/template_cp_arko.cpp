eeb0b6523097c15b899579b568d4830d00aad8d8
AAARKO7 <aadhikaryjs2@gmail.com>
2024-12-07 17:47:05 +0600

    commiting

--- templates/template_cp_arko.cpp
+++ templates/template_cp_arko.cpp
@@ -1,23 +1,22 @@
 /*
-    Solver:
-    Date:
+    author: Arko7
 
 */
 
-/**/
-
 #include<bits/stdc++.h>
 using namespace std;
+
+#define fast_IO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define int long long
 
 void solve()
 {
 
 }
 
-int main() 
+int32_t main() 
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+	fast_IO();
  
 	/*Test case*/
 	int t=1;
